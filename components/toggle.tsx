11e37f12208ef9741989d4a99603610c8315c6a1
bharadwaj palakurthy <bharadwajpalakurthy@gmail.com>
2023-08-01 22:56:24 +0530

    added design tokens

---
+++ components/toggle.tsx
@@ -0,0 +1,10 @@
+"use client";
+import { styled } from "styled-components";
+
+const Toggle = () => {
+  return <Styled>Toggle</Styled>;
+};
+
+export default Toggle;
+
+const Styled = styled.div``;
