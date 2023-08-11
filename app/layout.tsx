61881bb07a5fa00b7346d9f3e3636788f2c7e7eb
bharadwaj palakurthy <bharadwajpalakurthy@gmail.com>
2023-08-11 17:24:16 +0530

    added nav bar with hover effects

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import InfoBar from "@/components/info_bar/infoBar";
 import Footer from "@/components/footer/footer";
+import NavBar from "@/components/nav_bar/navBar";
 
 export const metadata: Metadata = {
   title: "Canto v3",
@@ -17,38 +18,37 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <StyledComponentsRegistry>
-          <InfoBar
-            values={[
-              {
-                name: "contracts w/ CSR enabled:",
-                value: "$1,210.56",
-                change: "+2% $23.4",
-                isPositive: true,
-              },
-              {
-                name: "CANTO price:",
-                value: "$1,210.56",
-                change: "+22%",
-                isPositive: true,
-              },
-              {
-                name: "TVL:",
-                value: "$1,210.56",
-                change: "-1.2%",
-                isPositive: false,
-              },
-              {
-                name: "Market Cap:",
-                value: "$1,435,438.56",
-                change: "-34.2%",
-                isPositive: false,
-              },
-            ]}
-          />
-          {children}
-          <Footer />
-        </StyledComponentsRegistry>
+        <InfoBar
+          values={[
+            {
+              name: "contracts w/ CSR enabled:",
+              value: "$1,210.56",
+              change: "+2% $23.4",
+              isPositive: true,
+            },
+            {
+              name: "CANTO price:",
+              value: "$1,210.56",
+              change: "+22%",
+              isPositive: true,
+            },
+            {
+              name: "TVL:",
+              value: "$1,210.56",
+              change: "-1.2%",
+              isPositive: false,
+            },
+            {
+              name: "Market Cap:",
+              value: "$1,435,438.56",
+              change: "-34.2%",
+              isPositive: false,
+            },
+          ]}
+        />
+        <NavBar />
+        {children}
+        <Footer />
       </body>
     </html>
   );
---
+++ components/nav_bar/navBar.tsx
@@ -0,0 +1,36 @@
+import Link from "next/link";
+import styles from "./navbar.module.scss";
+import Text from "../text";
+import Image from "next/image";
+
+const NavBar = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.logo}>
+        <Image src="/tokens/canto.svg" width={50} height={50} alt="canto" />
+      </div>
+
+      <div className={styles["nav-links"]}>
+        <Link href="/" className={styles["nav-link"]}>
+          <Text>Home</Text>
+        </Link>
+        <Link href="/bridge" className={styles["nav-link"]}>
+          <Text>Bridge</Text>
+        </Link>
+        <Link href="/staking" className={styles["nav-link"]}>
+          <Text>Staking</Text>
+        </Link>
+        <Link href="/lending" className={styles["nav-link"]}>
+          <Text>Lending</Text>
+        </Link>
+        <Link href="/governance" className={styles["nav-link"]}>
+          <Text>Governance</Text>
+        </Link>
+      </div>
+
+      <div className={styles["wallet-connect"]}></div>
+    </div>
+  );
+};
+
+export default NavBar;
