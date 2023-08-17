0f889f4f8fc32e27e1ac2fbb27694ed4c8f42d35
bharadwaj palakurthy <bharadwajpalakurthy@gmail.com>
2023-08-17 15:31:31 +0530

    added button component with loader and props

--- app/lending/page.tsx
+++ app/lending/page.tsx
@@ -1,7 +1,176 @@
+import Button from "@/components/button/button";
+import { url } from "inspector";
+
 export default function LendingPage() {
   return (
     <div>
       <h1>Lending Page</h1>
+
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(3, 1fr)",
+          placeItems: "center",
+          width: "50%px",
+          minHeight: "80%",
+        }}
+      >
+        <Button color="primary" height={"small"}>
+          Sample Text
+        </Button>
+        <Button color="secondary" height={"small"}>
+          Sample Text
+        </Button>
+        <Button color="accent" height={"small"}>
+          Sample Text
+        </Button>
+        <Button color="primary" height={"medium"}>
+          Sample Text
+        </Button>
+        <Button color="secondary" height={"medium"}>
+          Sample Text
+        </Button>
+        <Button color="accent" height={"medium"}>
+          Sample Text
+        </Button>
+        <Button color="primary" height={"large"}>
+          Sample Text
+        </Button>
+        <Button color="secondary" height={"large"}>
+          Sample Text
+        </Button>
+        <Button color="accent" height={"large"}>
+          Sample Text
+        </Button>
+        <Button color="primary" height={"small"} fontFamily="proto_mono">
+          Sample Text
+        </Button>
+        <Button color="secondary" height={"small"} fontFamily="proto_mono">
+          Sample Text
+        </Button>
+        <Button color="accent" height={"small"} fontFamily="proto_mono">
+          Sample Text
+        </Button>
+        <Button color="primary" height={"medium"} fontFamily="proto_mono">
+          Sample Text
+        </Button>
+        <Button color="secondary" height={"medium"} fontFamily="proto_mono">
+          Sample Text
+        </Button>
+        <Button color="accent" height={"medium"} fontFamily="proto_mono">
+          Sample Text
+        </Button>
+        <Button color="primary" height={"large"} fontFamily="proto_mono">
+          Sample Text
+        </Button>
+        <Button color="secondary" height={"large"} fontFamily="proto_mono">
+          Sample Text
+        </Button>
+        <Button color="accent" height={"large"} fontFamily="proto_mono">
+          Sample Text
+        </Button>
+
+        <Button
+          color="primary"
+          height={"medium"}
+          fontFamily="proto_mono"
+          padding={"sm"}
+        >
+          Sample Text
+        </Button>
+        <Button
+          color="secondary"
+          height={"medium"}
+          padding={"md"}
+          fontFamily="proto_mono"
+        >
+          Sample Text
+        </Button>
+        <Button
+          color="accent"
+          height={"medium"}
+          padding={"lg"}
+          fontFamily="proto_mono"
+        >
+          Sample Text
+        </Button>
+
+        <Button color="primary" isLoading fontFamily="proto_mono">
+          Sample Text
+        </Button>
+
+        <Button color="secondary" isLoading fontFamily="proto_mono">
+          Sample Text
+        </Button>
+        <Button color="accent" isLoading fontFamily="proto_mono">
+          Sample Text
+        </Button>
+        {/* icon - left */}
+        <Button
+          color="primary"
+          icon={{
+            url: "/tokens/note.svg",
+            position: "left",
+          }}
+          fontFamily="proto_mono"
+        >
+          Sample Text
+        </Button>
+
+        <Button
+          color="secondary"
+          icon={{
+            url: "/tokens/note.svg",
+            position: "left",
+          }}
+          fontFamily="proto_mono"
+        >
+          Sample Text
+        </Button>
+        <Button
+          color="accent"
+          icon={{
+            url: "/tokens/note.svg",
+            position: "left",
+          }}
+          fontFamily="proto_mono"
+        >
+          Sample Text
+        </Button>
+
+        {/* icon - right */}
+        <Button
+          color="primary"
+          icon={{
+            url: "/tokens/canto.svg",
+            position: "right",
+          }}
+          fontFamily="proto_mono"
+        >
+          Sample Text
+        </Button>
+
+        <Button
+          color="secondary"
+          icon={{
+            url: "/tokens/canto.svg",
+            position: "right",
+          }}
+          fontFamily="proto_mono"
+        >
+          Sample Text
+        </Button>
+        <Button
+          color="accent"
+          icon={{
+            url: "/tokens/canto.svg",
+            position: "right",
+          }}
+          fontFamily="proto_mono"
+        >
+          Sample Text
+        </Button>
+      </div>
     </div>
   );
 }
---
+++ components/button/button.tsx
@@ -0,0 +1,162 @@
+import localFont from "next/font/local";
+import styles from "./button.module.scss";
+import Image from "next/image";
+
+const rm_mono = localFont({
+  src: "../../fonts/rm-mono-regular.ttf",
+  weight: "300",
+  style: "normal",
+});
+
+const proto_mono = localFont({
+  src: "../../fonts/proto-mono-regular.ttf",
+  weight: "300",
+  style: "normal",
+});
+
+interface Props {
+  height?: "small" | "medium" | "large" | number;
+  width?: "contain" | "fill" | number;
+  color?: "primary" | "secondary" | "accent";
+  icon?: {
+    url: string;
+    position: "left" | "right";
+    size?: number;
+  };
+  padding?: "sm" | "md" | "lg" | number;
+  fontFamily?: "rm_mono" | "proto_mono";
+  weight?: "regular" | "bold";
+  children: React.ReactNode;
+  isLoading?: boolean;
+}
+
+const Button = (props: Props) => {
+  const getHeight = () => {
+    switch (props.height) {
+      case "small":
+        return 35;
+      case "medium":
+        return 46;
+      case "large":
+        return 56;
+      case undefined:
+        return 46;
+      default:
+        return props.height;
+    }
+  };
+
+  const getFontFamily = () => {
+    switch (props.fontFamily) {
+      case "rm_mono":
+        return rm_mono.style.fontFamily;
+      case "proto_mono":
+        return proto_mono.style.fontFamily;
+      default:
+        return rm_mono.style.fontFamily;
+    }
+  };
+
+  const getWidth = () => {
+    switch (props.width) {
+      case "contain":
+        return "fit-content";
+      case "fill":
+        return "100%";
+      case undefined:
+        return "fit-content";
+      default:
+        return props.width;
+    }
+  };
+
+  const getBGColor = () => {
+    switch (props.color) {
+      case "primary":
+        return "var(--text-dark-color)";
+      case "secondary":
+        return "var(--card-surface-color)";
+      case "accent":
+        return "var(--extra-success-color)";
+      case undefined:
+        return "var(--text-dark-color)";
+      default:
+        return props.color;
+    }
+  };
+
+  const getTextColor = () => {
+    switch (props.color) {
+      case "primary":
+        return "var(--text-light-color)";
+      case "secondary":
+        return "var(--text-dark-color)";
+      case "accent":
+        return "var(--text-dark-color)";
+      case undefined:
+        return "var(--text-light-color)";
+      default:
+        return props.color;
+    }
+  };
+
+  const getPadding = () => {
+    switch (props.padding) {
+      case "sm":
+        return 10;
+      case "md":
+        return 20;
+      case "lg":
+        return 30;
+      case undefined:
+        return 20;
+      default:
+        return props.padding;
+    }
+  };
+
+  return (
+    <div
+      className={styles.container}
+      style={{
+        height: getHeight() + "px",
+        width: getWidth(),
+        backgroundColor: getBGColor(),
+        padding: getPadding() + "px",
+        color: getTextColor(),
+        fontFamily: getFontFamily(),
+        gap: "12px",
+        fontWeight: props.weight == "bold" ? "bold" : "normal",
+        flexDirection: props.icon?.position == "right" ? "row-reverse" : "row",
+      }}
+    >
+      {props.icon && (
+        <Image
+          src={props.icon.url}
+          style={{
+            filter: props.color == "primary" ? "invert(1)" : "invert(0)",
+          }}
+          alt="icon"
+          width={props.icon.size || 16}
+          height={props.icon.size || 16}
+          className={styles.icon}
+        ></Image>
+      )}
+      {props.children}{" "}
+      {props.isLoading && (
+        <Image
+          style={{
+            filter: props.color == "primary" ? "invert(1)" : "invert(0)",
+          }}
+          src="/loader.svg"
+          alt="loading"
+          width={22}
+          height={22}
+          className={styles.loader}
+        ></Image>
+      )}
+    </div>
+  );
+};
+
+export default Button;
