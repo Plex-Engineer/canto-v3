c0d78ce732ed0129e4e247c6eeb45bcec41f9e29
bharadwaj palakurthy <bharadwajpalakurthy@gmail.com>
2023-08-23 16:37:34 +0530

    tweaked text component

--- components/text.tsx
+++ components/text.tsx
@@ -13,9 +13,9 @@ const proto_mono = localFont({
 });
 
 interface Props {
-  name?: "rm_mono" | "proto_mono";
+  font?: "rm_mono" | "proto_mono";
   weight?: "regular" | "bold";
-  size?: "xx-sm" | "x-sm" | "sm" | "reg" | "lg" | "x-lg";
+  size?: "xx-sm" | "x-sm" | "sm" | "md" | "lg" | "x-lg";
   opacity?: number;
   theme?:
     | "primary-light"
@@ -31,7 +31,7 @@ const sizes = {
   "xx-sm": 12,
   "x-sm": 14,
   sm: 16,
-  reg: 18,
+  md: 18,
   lg: 20,
   "x-lg": 24,
 };
@@ -44,7 +44,7 @@ const themes = {
 };
 
 const Text = ({
-  name,
+  font,
   weight,
   size,
   children,
@@ -57,12 +57,12 @@ const Text = ({
     <p
       style={{
         fontFamily:
-          name == "proto_mono"
+          font == "proto_mono"
             ? proto_mono.style.fontFamily
             : rm_mono.style.fontFamily,
         opacity: (opacity ?? 100) / 100,
         fontWeight: weight,
-        fontSize: sizes[size ?? "reg"],
+        fontSize: sizes[size ?? "md"],
         lineHeight: "140%",
         letterSpacing: "-0.32px",
         color: color == null ? themes[theme ?? "primary-dark"] : color,
