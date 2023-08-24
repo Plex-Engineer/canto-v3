f137c5f1cb8c28464eaa889fd6f6455c2049b3b4
dsudit01 <79417718+dsudit01@users.noreply.github.com>
2023-08-24 13:09:29 -0400

    revert rest api url

--- config/networks/cosmos/evmos.ts
+++ config/networks/cosmos/evmos.ts
@@ -13,8 +13,8 @@ export const EVMOS: CosmosNetwork = {
   icon: "https://raw.githubusercontent.com/cosmos/chain-registry/master/evmos/images/evmos.svg",
   name: "Evmos",
   isTestChain: false,
-  rpcUrl: "http://localhost:8010/proxy",
-  restEndpoint: "http://localhost:8010/proxy",
+  rpcUrl: "https://evmos.kingnodes.com",
+  restEndpoint: "https://evmos.kingnodes.com",
   addressPrefix: "evmos",
   checkAddress: function (address) {
     return checkCosmosAddress(this.addressPrefix)(address);
