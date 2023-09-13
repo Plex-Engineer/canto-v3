ade05b680204aac4d9b13461ccbf6bf6d34dfb42
dsudit01 <79417718+dsudit01@users.noreply.github.com>
2023-09-13 14:01:37 -0400

    format balance update with truncation

--- utils/tokenBalances.utils.ts
+++ utils/tokenBalances.utils.ts
@@ -44,22 +44,45 @@ export function formatBalance(
     commify?: boolean;
   }
 ): string {
-  const { symbol = "", precision = undefined, commify = false } = options || {};
+  // set this to avoid scientific notation
+  BigNumber.set({ EXPONENTIAL_AT: 25 });
+  const {
+    symbol = undefined,
+    precision = undefined,
+    commify = false,
+  } = options || {};
   const bnAmount = new BigNumber(amount);
+  // make sure greater than zero
+  if (bnAmount.isLessThanOrEqualTo(0)) return "0";
+  // divide by 10^decimals
   const formattedAmount = bnAmount.dividedBy(new BigNumber(10).pow(decimals));
   // if precision is undefined, ret2 places after the first non-zero decimal
-  const truncateAt =
+  let truncateAt =
     precision ??
     2 - Math.floor(Math.log(formattedAmount.toNumber()) / Math.log(10));
-  // make sure truncateAt is a positive number
-  const truncatedAmount = formattedAmount.toFixed(
-    Math.max(0, isFinite(truncateAt) ? truncateAt : 0)
-  );
-  return (
-    (commify
-      ? truncatedAmount.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")
-      : truncatedAmount) +
-    " " +
-    symbol
-  );
+  // make sure tuncation is not negative or greater than decimals
+  truncateAt = Math.max(0, Math.min(truncateAt, decimals));
+  // convert amount to string
+  const stringAmount = formattedAmount.toString();
+  // get index of the decimal in the string
+  const decimalIndex = stringAmount.indexOf(".");
+
+  // if decimal index is -1, there is no decimals, else truncate at the decimal index + truncateAt + 1
+  // if truncateAt is 0, then we want to only take the whole number
+  const truncatedAmount =
+    decimalIndex === -1
+      ? stringAmount
+      : stringAmount.slice(
+          0,
+          decimalIndex + truncateAt + (truncateAt === 0 ? 0 : 1)
+        );
+
+  // // create regex to truncate at the correct number of decimals
+  // const regex = new RegExp("^-?\\d+(?:.\\d{0," + truncateAt + "})?");
+  // const truncatedAmount = formattedAmount.toString().match(regex)?.[0] ?? "0";
+  return `${
+    commify
+      ? truncatedAmount.replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")
+      : truncatedAmount
+  }${symbol ? " " + symbol : ""}`;
 }
