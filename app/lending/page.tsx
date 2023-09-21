10b018ebc6a44d766c05ab99099e7ab6d03d11cb
bharadwaj palakurthy <bharadwajpalakurthy@gmail.com>
2023-09-21 14:29:34 +0530

    adding table

--- app/lending/page.tsx
+++ app/lending/page.tsx
@@ -1,206 +1,216 @@
 "use client";
 
-import Button from "@/components/button/button";
-import Modal from "@/components/modal/modal";
-import Text from "@/components/text";
-import { useState } from "react";
+import Table from "@/components/table/table";
 
 export default function LendingPage() {
-  const [isModalOpen, setIsModalOpen] = useState(false);
   return (
     <div>
-      <Modal
-        open={isModalOpen}
-        onClose={() => {
-          setIsModalOpen(false);
-        }}
-        width="500px"
-        height="80vh"
-      >
-        <Text font="proto_mono"> This is a text</Text>
-      </Modal>
-      <div
-        style={{
-          display: "grid",
-          gridTemplateColumns: "repeat(3, 1fr)",
-          placeItems: "center",
-          width: "50%px",
-          minHeight: "100%",
-          backgroundColor: "var(--card-sub-surface-color",
-        }}
-      >
-        {/* <Button
-          color="primary"
-          height={"small"}
-          onClick={() => {
-            setIsModalOpen(true);
-          }}
-        >
-          Show Modal
-        </Button> */}
-        <Button color="secondary" height={"small"}>
-          Sample Text
-        </Button>
-        <Button color="accent" height={"small"}>
-          Sample Text
-        </Button>
-        <Button color="primary" height={"medium"}>
-          Sample Text
-        </Button>
-
-        <Button color="secondary" height={"small"} disabled>
-          Sample Text
-        </Button>
-        <Button color="accent" height={"small"} disabled>
-          Sample Text
-        </Button>
-        <Button color="primary" height={"medium"} disabled>
-          Sample Text
-        </Button>
-        <Button color="secondary" height={"medium"}>
-          Sample Text
-        </Button>
-        <Button color="accent" height={"medium"}>
-          Sample Text
-        </Button>
-        <Button color="primary" height={"large"}>
-          Sample Text
-        </Button>
-        <Button color="secondary" height={"large"}>
-          Sample Text
-        </Button>
-        <Button color="accent" height={"large"}>
-          Sample Text
-        </Button>
-        <Button color="primary" height={"small"} fontFamily="proto_mono">
-          Sample Text
-        </Button>
-        <Button color="secondary" height={"small"} fontFamily="proto_mono">
-          Sample Text
-        </Button>
-        <Button color="accent" height={"small"} fontFamily="proto_mono">
-          Sample Text
-        </Button>
-        <Button color="primary" height={"medium"} fontFamily="proto_mono">
-          Sample Text
-        </Button>
-        <Button color="secondary" height={"medium"} fontFamily="proto_mono">
-          Sample Text
-        </Button>
-        <Button color="accent" height={"medium"} fontFamily="proto_mono">
-          Sample Text
-        </Button>
-        <Button color="primary" height={"large"} fontFamily="proto_mono">
-          Sample Text
-        </Button>
-        <Button color="secondary" height={"large"} fontFamily="proto_mono">
-          Sample Text
-        </Button>
-        <Button color="accent" height={"large"} fontFamily="proto_mono">
-          Sample Text
-        </Button>
-
-        <Button
-          color="primary"
-          height={"medium"}
-          fontFamily="proto_mono"
-          padding={"sm"}
-        >
-          Sample Text
-        </Button>
-        <Button
-          color="secondary"
-          height={"medium"}
-          padding={"md"}
-          fontFamily="proto_mono"
-        >
-          Sample Text
-        </Button>
-        <Button
-          color="accent"
-          height={"medium"}
-          padding={"lg"}
-          fontFamily="proto_mono"
-        >
-          Sample Text
-        </Button>
-
-        <Button color="primary" isLoading fontFamily="proto_mono">
-          Sample Text
-        </Button>
-
-        <Button color="secondary" isLoading fontFamily="proto_mono">
-          Sample Text
-        </Button>
-        <Button color="accent" isLoading fontFamily="proto_mono">
-          Sample Text
-        </Button>
-        {/* icon - left */}
-        <Button
-          color="primary"
-          icon={{
-            url: "/tokens/note.svg",
-            position: "left",
-          }}
-          fontFamily="proto_mono"
-        >
-          Sample Text
-        </Button>
-
-        <Button
-          color="secondary"
-          icon={{
-            url: "/tokens/note.svg",
-            position: "left",
-          }}
-          fontFamily="proto_mono"
-        >
-          Sample Text
-        </Button>
-        <Button
-          color="accent"
-          icon={{
-            url: "/tokens/note.svg",
-            position: "left",
-          }}
-          fontFamily="proto_mono"
-        >
-          Sample Text
-        </Button>
-
-        {/* icon - right */}
-        <Button
-          color="primary"
-          icon={{
-            url: "/tokens/canto.svg",
-            position: "right",
-          }}
-          fontFamily="proto_mono"
-        >
-          Sample Text
-        </Button>
-
-        <Button
-          color="secondary"
-          icon={{
-            url: "/tokens/canto.svg",
-            position: "right",
-          }}
-          fontFamily="proto_mono"
-        >
-          Sample Text
-        </Button>
-        <Button
-          color="accent"
-          icon={{
-            url: "/tokens/canto.svg",
-            position: "right",
-          }}
-          fontFamily="proto_mono"
-        >
-          Sample Text
-        </Button>
-      </div>
+      <Table
+        title="RWAS"
+        headers={[
+          "Asset",
+          "APR",
+          "Wallet Balance",
+          "Supplied Amount",
+          "Collateral Factor",
+          "",
+        ]}
+        data={[]}
+      />
     </div>
   );
+}
+
+//
+{
+  /* <div
+style={{
+  display: "grid",
+  gridTemplateColumns: "repeat(3, 1fr)",
+  placeItems: "center",
+  width: "50%px",
+  minHeight: "100%",
+  backgroundColor: "var(--card-sub-surface-color",
+}}
+> */
+}
+{
+  /* <Button
+  color="primary"
+  height={"small"}
+  onClick={() => {
+    setIsModalOpen(true);
+  }}
+>
+  Show Modal
+</Button> */
+}
+{
+  /* <Button color="secondary" height={"small"}>
+  Sample Text
+</Button>
+<Button color="accent" height={"small"}>
+  Sample Text
+</Button>
+<Button color="primary" height={"medium"}>
+  Sample Text
+</Button>
+
+<Button color="secondary" height={"small"} disabled>
+  Sample Text
+</Button>
+<Button color="accent" height={"small"} disabled>
+  Sample Text
+</Button>
+<Button color="primary" height={"medium"} disabled>
+  Sample Text
+</Button>
+<Button color="secondary" height={"medium"}>
+  Sample Text
+</Button>
+<Button color="accent" height={"medium"}>
+  Sample Text
+</Button>
+<Button color="primary" height={"large"}>
+  Sample Text
+</Button>
+<Button color="secondary" height={"large"}>
+  Sample Text
+</Button>
+<Button color="accent" height={"large"}>
+  Sample Text
+</Button>
+<Button color="primary" height={"small"} fontFamily="proto_mono">
+  Sample Text
+</Button>
+<Button color="secondary" height={"small"} fontFamily="proto_mono">
+  Sample Text
+</Button>
+<Button color="accent" height={"small"} fontFamily="proto_mono">
+  Sample Text
+</Button>
+<Button color="primary" height={"medium"} fontFamily="proto_mono">
+  Sample Text
+</Button>
+<Button color="secondary" height={"medium"} fontFamily="proto_mono">
+  Sample Text
+</Button>
+<Button color="accent" height={"medium"} fontFamily="proto_mono">
+  Sample Text
+</Button>
+<Button color="primary" height={"large"} fontFamily="proto_mono">
+  Sample Text
+</Button>
+<Button color="secondary" height={"large"} fontFamily="proto_mono">
+  Sample Text
+</Button>
+<Button color="accent" height={"large"} fontFamily="proto_mono">
+  Sample Text
+</Button>
+
+<Button
+  color="primary"
+  height={"medium"}
+  fontFamily="proto_mono"
+  padding={"sm"}
+>
+  Sample Text
+</Button>
+<Button
+  color="secondary"
+  height={"medium"}
+  padding={"md"}
+  fontFamily="proto_mono"
+>
+  Sample Text
+</Button>
+<Button
+  color="accent"
+  height={"medium"}
+  padding={"lg"}
+  fontFamily="proto_mono"
+>
+  Sample Text
+</Button>
+
+<Button color="primary" isLoading fontFamily="proto_mono">
+  Sample Text
+</Button>
+
+<Button color="secondary" isLoading fontFamily="proto_mono">
+  Sample Text
+</Button>
+<Button color="accent" isLoading fontFamily="proto_mono">
+  Sample Text
+</Button>
+{/* icon - left */
+}
+{
+  /* <Button
+  color="primary"
+  icon={{
+    url: "/tokens/note.svg",
+    position: "left",
+  }}
+  fontFamily="proto_mono"
+>
+  Sample Text
+</Button>
+
+<Button
+  color="secondary"
+  icon={{
+    url: "/tokens/note.svg",
+    position: "left",
+  }}
+  fontFamily="proto_mono"
+>
+  Sample Text
+</Button>
+<Button
+  color="accent"
+  icon={{
+    url: "/tokens/note.svg",
+    position: "left",
+  }}
+  fontFamily="proto_mono"
+>
+  Sample Text
+</Button>
+
+{/* icon - right */
+}
+{
+  /* <Button
+  color="primary"
+  icon={{
+    url: "/tokens/canto.svg",
+    position: "right",
+  }}
+  fontFamily="proto_mono"
+>
+  Sample Text
+</Button>
+
+<Button
+  color="secondary"
+  icon={{
+    url: "/tokens/canto.svg",
+    position: "right",
+  }}
+  fontFamily="proto_mono"
+>
+  Sample Text
+</Button>
+<Button
+  color="accent"
+  icon={{
+    url: "/tokens/canto.svg",
+    position: "right",
+  }}
+  fontFamily="proto_mono"
+>
+  Sample Text
+</Button>
+</div> */
 }
---
+++ components/table/table.tsx
@@ -0,0 +1,44 @@
+import styles from "./table.module.scss";
+
+interface Props {
+  title?: string;
+  headers: string[];
+  data: string[][];
+}
+
+function formatTitle(title: string) {
+  return title
+    .replace(/_/g, " ")
+    .replace(/\w\S*/g, (w) => w.replace(/^\w/, (c) => c.toUpperCase()));
+}
+
+const Table = (props: Props) => {
+  return (
+    <div className={styles.table}>
+      <div className={styles.row + " " + styles.header}>
+        {props.headers.map((header, index) => {
+          return (
+            <div key={index} className={styles.cell}>
+              {formatTitle(header)}
+            </div>
+          );
+        })}
+      </div>
+      {props.data.map((row, index) => {
+        return (
+          <div key={index} className={styles.row}>
+            {row.map((cell, index) => {
+              return (
+                <div key={index} className={styles.cell}>
+                  cell
+                </div>
+              );
+            })}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Table;
