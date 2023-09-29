0447821db97ea2dad93c06dcc93632ad60ff3da1
dsudit01 <79417718+dsudit01@users.noreply.github.com>
2023-09-29 12:19:30 -0400

    function comments natspec

--- utils/formatting.utils.ts
+++ utils/formatting.utils.ts
@@ -55,6 +55,11 @@ export function formatError(errorMsg: string): string {
   return "Error:" + split[split.length - 1];
 }
 
+/**
+ * @notice Formats a number into a percentage
+ * @param {string} percent percentage to format
+ * @returns {string} Formatted percentage
+ */
 export function formatPercent(percent: string): string {
   return (parseFloat(percent) * 100).toFixed(2) + "%";
 }
