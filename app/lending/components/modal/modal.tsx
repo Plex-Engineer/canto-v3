32eeec7711a39c30b6420cfc8f8a8e1e58f1d4ee
bharadwaj palakurthy <bharadwajpalakurthy@gmail.com>
2023-10-04 02:36:04 +0530

    conditional rendering for APR's

--- app/lending/components/modal/modal.tsx
+++ app/lending/components/modal/modal.tsx
@@ -94,6 +94,7 @@ export const LendingModal = (props: Props) => {
     isSupply: boolean;
   }) => (
     <Container className={styles.card} padding="md" width="100%">
+      {/* might need to change this in future for showing it on more tokens */}
       {isSupply && cToken.symbol.toLowerCase() == "cnote" && (
         <>
           <Card name="Supply APR" value={cToken.supplyApy + "%"} />
