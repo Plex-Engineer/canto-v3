5cef83d51b1aa47b7ab365c3e369888b0777840e
bharadwaj palakurthy <bharadwajpalakurthy@gmail.com>
2023-10-13 17:39:57 +0530

    added eco system

---
+++ app/components/ecoTile.tsx
@@ -0,0 +1,45 @@
+import Text from "@/components/text";
+import styles from "./ecoTile.module.scss";
+import Image from "next/image";
+
+interface Props {
+  name: string;
+  description: string;
+  image: string;
+  link: string;
+}
+
+const EcoTile = (props: Props) => {
+  return (
+    <div className={styles.card}>
+      <a href={props.link} target="_blank" className={styles["card-title"]}>
+        <Text font="proto_mono" size="x-lg" theme="primary-light">
+          {props.name}
+        </Text>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="25"
+          height="25"
+          viewBox="0 0 25 25"
+          fill="none"
+        >
+          <path
+            d="M7.28135 4.46094H20.4813M20.4813 4.46094V17.6609M20.4813 4.46094L4.88135 20.0609"
+            stroke="var(--text-light-color)"
+            stroke-width="2"
+            stroke-linejoin="round"
+          />
+        </svg>
+      </a>
+
+      <div className={styles["card-image"]}>
+        <Image src={props.image} alt={props.name} width={300} height={300} />
+      </div>
+      <Text size="sm" theme="secondary-dark" className={styles.desc}>
+        {props.description}
+      </Text>
+    </div>
+  );
+};
+
+export default EcoTile;
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,8 @@
 
 import Container from "@/components/container/container";
 import Text from "@/components/text";
-
+import styles from "./home.module.scss";
+import EcoTile from "./components/ecoTile";
 export default function Home() {
   return (
     <Container
@@ -14,6 +15,42 @@ export default function Home() {
       <Text font="proto_mono" size="x-lg">
         Welcome to Canto-v3
       </Text>
+
+      <section className={styles.ecosystem}>
+        <Text font="proto_mono" size="title">
+          Ecosystem
+        </Text>
+
+        <div className={styles["eco-grid"]}>
+          <EcoTile
+            name="Blank Rasa"
+            description="Buy and Sell NFTs"
+            image="/ecosystem/blank-rasa.svg"
+            link="https://www.blankrasa.com/"
+          />
+
+          <EcoTile
+            name="Slingshot"
+            description="Swap tokens on Canto and 8 other networks"
+            image="/ecosystem/slingshot.svg"
+            link="https://slingshot.finance/"
+          />
+
+          <EcoTile
+            name="Cadence Protocol"
+            description="Next-gen decentralized perpetuals"
+            image="/ecosystem/cad.png"
+            link="https://www.cadenceprotocol.io/"
+          />
+
+          <EcoTile
+            name="Vivacity (coming soon)"
+            description="Yet to be announced"
+            image="/ecosystem/blankRasa.svg"
+            link="https://blankrasa.io/"
+          />
+        </div>
+      </section>
     </Container>
   );
 }
