8867b8e374f2aee0365110ba322817c988435ac0
bharadwaj palakurthy <bharadwajpalakurthy@gmail.com>
2023-10-26 23:23:24 +0530

    added styling for showing advanced

--- app/lp/components/ambient/newAmbientPosition.tsx
+++ app/lp/components/ambient/newAmbientPosition.tsx
@@ -31,209 +31,259 @@ export const NewAmbientPositionModal = ({
   const [showAdvanced, setShowAdvanced] = useState(false);
   return (
     <Container width={showAdvanced ? "64rem" : "32rem"}>
-      <div className={styles.iconTitle}>
-        <Icon icon={{ url: pool.logoURI, size: 60 }} />
-        <Text size="lg" font="proto_mono">
-          {pool.symbol}
-        </Text>
-      </div>
+      <Container direction="row" gap={20}>
+        <Container>
+          <div className={styles.iconTitle}>
+            <Icon icon={{ url: pool.logoURI, size: 60 }} />
+            <Text size="lg" font="proto_mono">
+              {pool.symbol}
+            </Text>
+          </div>
 
-      <Spacer height="4px" />
-      <Amount
-        decimals={baseToken.decimals}
-        value={positionManager.options.amountBase}
-        onChange={(e) =>
-          positionManager.setters.setAmount(e.target.value, true)
-        }
-        IconUrl={baseToken.logoURI}
-        title={baseToken.symbol}
-        max={baseToken.balance ?? "0"}
-        symbol={baseToken.symbol}
-      />
-      <Spacer height="10px" />
-      <Amount
-        decimals={quoteToken.decimals}
-        value={positionManager.options.amountQuote}
-        onChange={(e) =>
-          positionManager.setters.setAmount(e.target.value, false)
-        }
-        IconUrl={quoteToken.logoURI}
-        title={quoteToken.symbol}
-        max={quoteToken.balance ?? "0"}
-        symbol={quoteToken.symbol}
-      />
-      <Spacer height="10px" />
-      <Container className={styles.card}>
-        <ModalItem
-          name="Current Price"
-          value={
-            displayAmount(
-              pool.stats.lastPriceSwap.toString(),
-              pool.base.decimals - pool.quote.decimals,
-              {
-                precision: 3,
+          <Spacer height="4px" />
+          <Amount
+            decimals={baseToken.decimals}
+            value={positionManager.options.amountBase}
+            onChange={(e) =>
+              positionManager.setters.setAmount(e.target.value, true)
+            }
+            IconUrl={baseToken.logoURI}
+            title={baseToken.symbol}
+            max={baseToken.balance ?? "0"}
+            symbol={baseToken.symbol}
+          />
+          <Spacer height="10px" />
+          <Amount
+            decimals={quoteToken.decimals}
+            value={positionManager.options.amountQuote}
+            onChange={(e) =>
+              positionManager.setters.setAmount(e.target.value, false)
+            }
+            IconUrl={quoteToken.logoURI}
+            title={quoteToken.symbol}
+            max={quoteToken.balance ?? "0"}
+            symbol={quoteToken.symbol}
+          />
+          <Spacer height="10px" />
+          <Container className={styles.card}>
+            <ModalItem
+              name="Current Price"
+              value={
+                displayAmount(
+                  pool.stats.lastPriceSwap.toString(),
+                  pool.base.decimals - pool.quote.decimals,
+                  {
+                    precision: 3,
+                  }
+                ) +
+                " " +
+                pool.base.symbol +
+                " = 1 " +
+                pool.quote.symbol
               }
-            ) +
-            " " +
-            pool.base.symbol +
-            " = 1 " +
-            pool.quote.symbol
-          }
-        />
-        <ModalItem
-          name="Fee"
-          value={
-            <Container>
-              <PopUp
-                content={
-                  <Text>Liquidity providers will receive fee on swaps</Text>
-                }
-                width="300px"
-              >
-                <Container
-                  style={{ display: "flex", flexDirection: "row", gap: "6px" }}
-                >
-                  <span className={styles.infoPop}>
-                    <Text
-                      theme="secondary-dark"
-                      size="sm"
+            />
+            <ModalItem
+              name="Fee"
+              value={
+                <Container>
+                  <PopUp
+                    content={
+                      <Text>Liquidity providers will receive fee on swaps</Text>
+                    }
+                    width="300px"
+                  >
+                    <Container
                       style={{
-                        textAlign: "right",
+                        display: "flex",
+                        flexDirection: "row",
+                        gap: "6px",
                       }}
                     >
-                      ?
-                    </Text>
-                  </span>
-                  <Text>{formatPercent(pool.stats.feeRate.toString())}</Text>
+                      <span className={styles.infoPop}>
+                        <Text
+                          theme="secondary-dark"
+                          size="sm"
+                          style={{
+                            textAlign: "right",
+                          }}
+                        >
+                          ?
+                        </Text>
+                      </span>
+                      <Text>
+                        {formatPercent(pool.stats.feeRate.toString())}
+                      </Text>
+                    </Container>
+                  </PopUp>
                 </Container>
-              </PopUp>
-            </Container>
-          }
-        />
-      </Container>
-      <Spacer height="8px" />
-      <Text size="x-sm" theme="secondary-dark">
-        This is a concentrated liquidity stable pool. The default range below is
-        selected for optimal rewards. Rewards will be released in weekly epochs.
-      </Text>
-      <Spacer height="8px" />
-      <Container className={styles.card}>
-        <ModalItem
-          name="Min Range Price: "
-          value={
-            <Container
-              center={{
-                vertical: true,
-              }}
-              gap={10}
-              direction="row"
-              style={{
-                width: "100px",
-              }}
-            >
-              <Input
-                height={"sm"}
-                type="number"
-                value={positionManager.options.minRangePrice}
-                onChange={(e) => {
-                  positionManager.setters.setRangePrice(e.target.value, true);
-                }}
-              />
-            </Container>
-          }
-        />
-        <ModalItem
-          name="Max Range Price: "
-          value={
-            <Container
-              center={{
-                vertical: true,
-              }}
-              gap={10}
-              direction="row"
-              style={{
-                width: "100px",
-              }}
-            >
-              <Input
-                height={"sm"}
-                type="number"
-                value={positionManager.options.maxRangePrice}
-                onChange={(e) => {
-                  positionManager.setters.setRangePrice(e.target.value, false);
-                }}
-              />
-            </Container>
-          }
-        />
-        <ModalItem
-          name="Min Execution Price: "
-          value={
-            <Container
-              center={{
-                vertical: true,
-              }}
-              gap={10}
-              direction="row"
-              style={{
-                width: "100px",
-              }}
-            >
-              <Input
-                height={"sm"}
-                type="number"
-                value={positionManager.options.minExecutionPrice}
-                onChange={(e) => {
-                  positionManager.setters.setExecutionPrice(
-                    e.target.value,
-                    true
-                  );
-                }}
-              />
-            </Container>
-          }
-        />
-        <Container>
-          <Toggle value={showAdvanced} onChange={setShowAdvanced} />
-        </Container>
+              }
+            />
+          </Container>
+          <Spacer height="8px" />
+          <Text size="x-sm" theme="secondary-dark">
+            This is a concentrated liquidity stable pool. The default range
+            below is selected for optimal rewards. Rewards will be released in
+            weekly epochs.
+          </Text>
+          <Spacer height="8px" />
+          <Container
+            direction="row"
+            gap={10}
+            center={{
+              horizontal: true,
+              vertical: true,
+            }}
+          >
+            <Text theme="secondary-dark" size="x-sm">
+              Advanced
+            </Text>{" "}
+            <Toggle value={showAdvanced} onChange={setShowAdvanced} />
+          </Container>
+          <Spacer height="8px" />
 
-        <ModalItem
-          name="Max Execution Price: "
-          value={
-            <Container
-              center={{
-                vertical: true,
-              }}
-              gap={10}
-              direction="row"
-              style={{
-                width: "100px",
-              }}
-            >
-              <Input
-                height={"sm"}
-                type="number"
-                value={positionManager.options.maxExecutionPrice}
-                onChange={(e) => {
-                  positionManager.setters.setExecutionPrice(
-                    e.target.value,
-                    false
-                  );
-                }}
-              />
-            </Container>
-          }
-        />
+          <Container className={styles.card}>
+            <ModalItem
+              name="Min Range Price: "
+              value={
+                <Container
+                  center={{
+                    vertical: true,
+                  }}
+                  gap={10}
+                  direction="row"
+                  style={{
+                    width: "100px",
+                  }}
+                >
+                  <Input
+                    height={"sm"}
+                    type="number"
+                    value={positionManager.options.minRangePrice}
+                    onChange={(e) => {
+                      positionManager.setters.setRangePrice(
+                        e.target.value,
+                        true
+                      );
+                    }}
+                  />
+                </Container>
+              }
+            />
+            <ModalItem
+              name="Max Range Price: "
+              value={
+                <Container
+                  center={{
+                    vertical: true,
+                  }}
+                  gap={10}
+                  direction="row"
+                  style={{
+                    width: "100px",
+                  }}
+                >
+                  <Input
+                    height={"sm"}
+                    type="number"
+                    value={positionManager.options.maxRangePrice}
+                    onChange={(e) => {
+                      positionManager.setters.setRangePrice(
+                        e.target.value,
+                        false
+                      );
+                    }}
+                  />
+                </Container>
+              }
+            />
+            <ModalItem
+              name="Min Execution Price: "
+              value={
+                <Container
+                  center={{
+                    vertical: true,
+                  }}
+                  gap={10}
+                  direction="row"
+                  style={{
+                    width: "100px",
+                  }}
+                >
+                  <Input
+                    height={"sm"}
+                    type="number"
+                    value={positionManager.options.minExecutionPrice}
+                    onChange={(e) => {
+                      positionManager.setters.setExecutionPrice(
+                        e.target.value,
+                        true
+                      );
+                    }}
+                  />
+                </Container>
+              }
+            />
+
+            <ModalItem
+              name="Max Execution Price: "
+              value={
+                <Container
+                  center={{
+                    vertical: true,
+                  }}
+                  gap={10}
+                  direction="row"
+                  style={{
+                    width: "100px",
+                  }}
+                >
+                  <Input
+                    height={"sm"}
+                    type="number"
+                    value={positionManager.options.maxExecutionPrice}
+                    onChange={(e) => {
+                      positionManager.setters.setExecutionPrice(
+                        e.target.value,
+                        false
+                      );
+                    }}
+                  />
+                </Container>
+              }
+            />
+          </Container>
+          <Text
+            style={{
+              opacity: !positionValidation.isValid ? 1 : 0,
+              color: "var(--extra-failure-color, #ff0000)",
+            }}
+            size="x-sm"
+          >
+            {positionValidation.errorMessage}
+          </Text>
+        </Container>
+        {showAdvanced && (
+          <Container className={styles.advancedContainer}>
+            <Text>Set Price Range</Text>
+            <Text size="x-sm">
+              Current Price :{" "}
+              {displayAmount(
+                pool.stats.lastPriceSwap.toString(),
+                pool.base.decimals - pool.quote.decimals,
+                {
+                  precision: 3,
+                }
+              ) +
+                " " +
+                pool.base.symbol +
+                " = 1 " +
+                pool.quote.symbol}
+            </Text>
+            <Spacer height="8px" />
+            <div className={styles.priceRanger}></div>
+          </Container>
+        )}
       </Container>
-      <Text
-        style={{
-          opacity: !positionValidation.isValid ? 1 : 0,
-          color: "var(--extra-failure-color, #ff0000)",
-        }}
-        size="x-sm"
-      >
-        {positionValidation.errorMessage}
-      </Text>
       <Spacer height="30px" />
       <Button
         disabled={!positionValidation.isValid}
