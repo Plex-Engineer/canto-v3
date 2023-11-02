53eb981ae30b63d1292312b9084945bee4e805ac
bharadwaj palakurthy <bharadwajpalakurthy@gmail.com>
2023-11-03 05:26:16 +0530

    added icons to graph

--- components/liquidityGraph/ambientLiquidityGraph.tsx
+++ components/liquidityGraph/ambientLiquidityGraph.tsx
@@ -6,6 +6,7 @@ import Button from "../button/button";
 import Container from "../container/container";
 import Spacer from "../layout/spacer";
 import Text from "../text";
+import Icon from "../icon/icon";
 
 interface Props {
   points: { x: number; y: number }[];
@@ -69,9 +70,11 @@ const AmbientLiquidityGraph = ({
       <Container direction="row" gap={"auto"} width="100%">
         <Text>Set Price Range</Text>
         <Spacer height="8px" />
-        <Container direction="row">
+        <Container direction="row" gap={4}>
           <ButtonHold
             color="secondary"
+            height={"small"}
+            width={20}
             onHold={() =>
               setXAxis((prev) => ({
                 min: prev.min - 0.0001,
@@ -80,9 +83,14 @@ const AmbientLiquidityGraph = ({
             }
             interval={50}
           >
-            -
+            <Icon
+              themed
+              icon={{ url: "/zoom-out.svg", size: { width: 24, height: 24 } }}
+            />
           </ButtonHold>
           <ButtonHold
+            height={"small"}
+            width={20}
             color="secondary"
             onHold={() =>
               setXAxis((prev) => ({
@@ -92,13 +100,21 @@ const AmbientLiquidityGraph = ({
             }
             interval={50}
           >
-            +
+            <Icon
+              themed
+              icon={{ url: "/zoom-in.svg", size: { width: 24, height: 24 } }}
+            />
           </ButtonHold>
           <Button
+            height={"small"}
+            width={20}
             color="secondary"
             onClick={() => setXAxis(initialXAxis(points))}
           >
-            RESET
+            <Icon
+              themed
+              icon={{ url: "/reset.svg", size: { width: 28, height: 28 } }}
+            />
           </Button>
         </Container>
       </Container>
