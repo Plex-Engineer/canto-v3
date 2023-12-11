df9513f3921c24c1236434857ad5889aa9907c38
dsudit01 <79417718+dsudit01@users.noreply.github.com>
2023-12-11 18:22:12 -0500

    fix error check on user clm data

--- hooks/lending/helpers/clmLens.ts
+++ hooks/lending/helpers/clmLens.ts
@@ -8,7 +8,6 @@ import { UserCTokenDetails } from "@/hooks/lending/interfaces/tokens";
 import { isValidEthAddress } from "@/utils/address";
 import { newContractInstance } from "@/utils/evm";
 import { CLM_LENS_ABI, COMPTROLLER_ABI } from "@/config/abis";
-import { isCantoChainId } from "@/utils/networks";
 import { getCantoCoreAddress } from "@/config/consts/addresses";
 
 /**
@@ -27,69 +26,66 @@ export async function getUserCLMLensData(
   limits: { liquidity: number; shortfall: number };
   compAccrued: number;
 }> {
-  if (isValidEthAddress(userEthAddress) || !isCantoChainId(chainId)) {
-    try {
-      // get all addresses depending on chainId
-      const [lensAddress, comptrollerAddress] = [
-        getCantoCoreAddress(chainId, "clmLens"),
-        getCantoCoreAddress(chainId, "comptroller"),
-      ];
-      // make sure addresses exist
-      if (!cTokenAddresses || !lensAddress || !comptrollerAddress) {
-        throw Error("getUserCLMLensData: chainId not supported");
-      }
-      // get lens contract
-      const { data: lensContract, error: lensError } = newContractInstance<
-        typeof CLM_LENS_ABI
-      >(chainId, lensAddress, CLM_LENS_ABI);
-      if (lensError) throw lensError;
-
-      // get comptroller contract
-      const { data: comptrollerContract, error: comptrollerError } =
-        newContractInstance<typeof COMPTROLLER_ABI>(
-          chainId,
-          comptrollerAddress,
-          COMPTROLLER_ABI
-        );
-      if (comptrollerError) throw comptrollerError;
-
-      const [cTokens, limits, compAccrued] = await Promise.all([
-        (
-          await lensContract.methods
-            .cTokenBalancesAll(
-              comptrollerAddress,
-              cTokenAddresses,
-              userEthAddress
-            )
-            .call()
-        ).map((data) => {
-          return {
-            chainId,
-            cTokenAddress: data.cTokenAddress,
-            balanceOfCToken: data.balanceOfCToken.toString(),
-            balanceOfUnderlying: data.balanceOfUnderlying.toString(),
-            borrowBalance: data.borrowBalance.toString(),
-            rewards: data.rewards.toString(),
-            isCollateral: data.isCollateral,
-            supplyBalanceInUnderlying:
-              data.supplyBalanceInUnderlying.toString(),
-            underlyingAllowance: data.underlyingAllowance.toString(),
-          };
-        }),
-        lensContract.methods
-          .getAccountLimits(comptrollerAddress, userEthAddress)
-          .call() as Promise<{ liquidity: number; shortfall: number }>,
-        comptrollerContract.methods
-          .compAccrued(userEthAddress)
-          .call() as Promise<number>,
-      ]);
-
-      return NO_ERROR({ cTokens, limits, compAccrued });
-    } catch (err) {
-      return NEW_ERROR("getUserCLMLensData: " + errMsg(err));
+  try {
+    if (!isValidEthAddress(userEthAddress)) {
+      throw Error("getUserCLMLensData: invalid userEthAddress");
     }
+    // get all addresses depending on chainId
+    const [lensAddress, comptrollerAddress] = [
+      getCantoCoreAddress(chainId, "clmLens"),
+      getCantoCoreAddress(chainId, "comptroller"),
+    ];
+    // make sure addresses exist
+    if (!cTokenAddresses || !lensAddress || !comptrollerAddress) {
+      throw Error("getUserCLMLensData: chainId not supported");
+    }
+    // get lens contract
+    const { data: lensContract, error: lensError } = newContractInstance<
+      typeof CLM_LENS_ABI
+    >(chainId, lensAddress, CLM_LENS_ABI);
+    if (lensError) throw lensError;
+
+    // get comptroller contract
+    const { data: comptrollerContract, error: comptrollerError } =
+      newContractInstance<typeof COMPTROLLER_ABI>(
+        chainId,
+        comptrollerAddress,
+        COMPTROLLER_ABI
+      );
+    if (comptrollerError) throw comptrollerError;
+
+    const [cTokens, limits, compAccrued] = await Promise.all([
+      (
+        await lensContract.methods
+          .cTokenBalancesAll(
+            comptrollerAddress,
+            cTokenAddresses,
+            userEthAddress
+          )
+          .call()
+      ).map((data) => {
+        return {
+          chainId,
+          cTokenAddress: data.cTokenAddress,
+          balanceOfCToken: data.balanceOfCToken.toString(),
+          balanceOfUnderlying: data.balanceOfUnderlying.toString(),
+          borrowBalance: data.borrowBalance.toString(),
+          rewards: data.rewards.toString(),
+          isCollateral: data.isCollateral,
+          supplyBalanceInUnderlying: data.supplyBalanceInUnderlying.toString(),
+          underlyingAllowance: data.underlyingAllowance.toString(),
+        };
+      }),
+      lensContract.methods
+        .getAccountLimits(comptrollerAddress, userEthAddress)
+        .call() as Promise<{ liquidity: number; shortfall: number }>,
+      comptrollerContract.methods
+        .compAccrued(userEthAddress)
+        .call() as Promise<number>,
+    ]);
+
+    return NO_ERROR({ cTokens, limits, compAccrued });
+  } catch (err) {
+    return NEW_ERROR("getUserCLMLensData: " + errMsg(err));
   }
-  return NEW_ERROR(
-    "getUserCLMLensData: Invalid Params: " + userEthAddress + " " + chainId
-  );
 }
