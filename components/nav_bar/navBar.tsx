43259b3c8da4e709d4cee91e09f59e13420544a6
dsudit01 <79417718+dsudit01@users.noreply.github.com>
2024-01-22 15:48:20 +0530

    readd explore page

--- components/nav_bar/navBar.tsx
+++ components/nav_bar/navBar.tsx
@@ -130,6 +130,16 @@ const NavBar = () => {
         >
           <Text size="sm">Governance</Text>
         </Link>
+
+        <Link
+          href="/explore"
+          className={clsx(
+            styles["nav-link"],
+            currentPath == "/explore" && styles.active
+          )}
+        >
+          <Text size="sm">Explore</Text>
+        </Link>
       </div>
       <div className={styles["btn-grp"]}>
         <div className={styles.theme}>
