261436144744816436a4eff586cb0b3549e8a8f4
Mohd Asaduddin Amaan <83865119+Asaduddin18@users.noreply.github.com>
2024-02-29 17:08:18 +0530

    add unit tests for countdown component

---
+++ __tests__/components/countdown.test.tsx
@@ -0,0 +1,25 @@
+import '@testing-library/jest-dom';
+import { render, act } from '@testing-library/react';
+import Countdown from "@/components/timer/countdown";
+
+jest.useFakeTimers();
+
+describe('Countdown', () => {
+  const RealDateNow = Date.now;
+
+  beforeAll(() => {
+    const mockDateNow = jest.fn(() => 0);
+    global.Date.now = mockDateNow;
+  });
+
+  afterAll(() => {
+    global.Date.now = RealDateNow;
+  });
+
+  it('renders the time left in the default format', () => {
+    const { getByText } = render(<Countdown endTimestamp={BigInt(1000 * 60 * 60 * 24 * 2)} />);
+
+    expect(getByText('2 Days : 0 Hours : 0 Minutes : 0 Seconds')).toBeInTheDocument();
+  });
+
+});
